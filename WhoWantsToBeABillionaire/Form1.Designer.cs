b8003a3b62f826a8e25af3b545e5511c531d49eb
vboxuser <vboxuser@WIN10.ty>
2025-02-17 19:11:58 +0300

    Before SQL addition

--- WhoWantsToBeABillionaire/Form1.Designer.cs
+++ WhoWantsToBeABillionaire/Form1.Designer.cs
@@ -34,13 +34,15 @@
             this.btnAnswerC = new System.Windows.Forms.Button();
             this.btnAnswerD = new System.Windows.Forms.Button();
             this.lblQuestion = new System.Windows.Forms.Label();
+            this.lstLevel = new System.Windows.Forms.ListBox();
+            this.bntFiftyFifty = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::WhoWantsToBeABillionaire.Properties.Resources.wwtbab_pict;
-            this.pictureBox1.Location = new System.Drawing.Point(98, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(49, 12);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(367, 225);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -49,57 +51,100 @@
             // 
             // btnAnswerA
             // 
-            this.btnAnswerA.Location = new System.Drawing.Point(98, 338);
+            this.btnAnswerA.Location = new System.Drawing.Point(49, 340);
             this.btnAnswerA.Name = "btnAnswerA";
-            this.btnAnswerA.Size = new System.Drawing.Size(75, 23);
+            this.btnAnswerA.Size = new System.Drawing.Size(136, 42);
             this.btnAnswerA.TabIndex = 1;
             this.btnAnswerA.Tag = "1";
             this.btnAnswerA.Text = "button1";
             this.btnAnswerA.UseVisualStyleBackColor = false;
+            this.btnAnswerA.Click += new System.EventHandler(this.btnAnswerA_Click);
             // 
             // btnAnswerB
             // 
-            this.btnAnswerB.Location = new System.Drawing.Point(98, 386);
+            this.btnAnswerB.Location = new System.Drawing.Point(49, 388);
             this.btnAnswerB.Name = "btnAnswerB";
-            this.btnAnswerB.Size = new System.Drawing.Size(75, 23);
+            this.btnAnswerB.Size = new System.Drawing.Size(136, 38);
             this.btnAnswerB.TabIndex = 2;
             this.btnAnswerB.Tag = "2";
             this.btnAnswerB.Text = "button2";
             this.btnAnswerB.UseVisualStyleBackColor = true;
+            this.btnAnswerB.Click += new System.EventHandler(this.btnAnswerB_Click);
             // 
             // btnAnswerC
             // 
-            this.btnAnswerC.Location = new System.Drawing.Point(341, 338);
+            this.btnAnswerC.Location = new System.Drawing.Point(191, 340);
             this.btnAnswerC.Name = "btnAnswerC";
-            this.btnAnswerC.Size = new System.Drawing.Size(75, 23);
+            this.btnAnswerC.Size = new System.Drawing.Size(132, 42);
             this.btnAnswerC.TabIndex = 3;
             this.btnAnswerC.Tag = "3";
             this.btnAnswerC.Text = "button3";
             this.btnAnswerC.UseVisualStyleBackColor = true;
+            this.btnAnswerC.Click += new System.EventHandler(this.btnAnswerC_Click);
             // 
             // btnAnswerD
             // 
-            this.btnAnswerD.Location = new System.Drawing.Point(341, 386);
+            this.btnAnswerD.Location = new System.Drawing.Point(191, 388);
             this.btnAnswerD.Name = "btnAnswerD";
-            this.btnAnswerD.Size = new System.Drawing.Size(75, 23);
+            this.btnAnswerD.Size = new System.Drawing.Size(132, 38);
             this.btnAnswerD.TabIndex = 4;
             this.btnAnswerD.Tag = "4";
             this.btnAnswerD.Text = "button4";
             this.btnAnswerD.UseVisualStyleBackColor = true;
+            this.btnAnswerD.Click += new System.EventHandler(this.btnAnswerD_Click);
             // 
             // lblQuestion
             // 
-            this.lblQuestion.Location = new System.Drawing.Point(138, 266);
+            this.lblQuestion.Location = new System.Drawing.Point(22, 269);
             this.lblQuestion.Name = "lblQuestion";
             this.lblQuestion.Size = new System.Drawing.Size(278, 41);
             this.lblQuestion.TabIndex = 5;
             this.lblQuestion.Text = "label1";
+            // 
+            // lstLevel
+            // 
+            this.lstLevel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lstLevel.FormattingEnabled = true;
+            this.lstLevel.ItemHeight = 16;
+            this.lstLevel.Items.AddRange(new object[] {
+            "3 000 000",
+            "1 500 000",
+            "800 000",
+            "400 000",
+            "200 000",
+            "100 000",
+            "50 000",
+            "25 000",
+            "15 000",
+            "10 000",
+            "5 000",
+            "3 000",
+            "2 000",
+            "1 000",
+            "500"});
+            this.lstLevel.Location = new System.Drawing.Point(474, 32);
+            this.lstLevel.Name = "lstLevel";
+            this.lstLevel.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lstLevel.Size = new System.Drawing.Size(75, 260);
+            this.lstLevel.TabIndex = 6;
+            // 
+            // bntFiftyFifty
+            // 
+            this.bntFiftyFifty.Location = new System.Drawing.Point(424, 347);
+            this.bntFiftyFifty.Name = "bntFiftyFifty";
+            this.bntFiftyFifty.Size = new System.Drawing.Size(75, 23);
+            this.bntFiftyFifty.TabIndex = 7;
+            this.bntFiftyFifty.Text = "50/50";
+            this.bntFiftyFifty.UseVisualStyleBackColor = true;
+            this.bntFiftyFifty.Click += new System.EventHandler(this.bntFiftyFifty_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(583, 438);
+            this.Controls.Add(this.bntFiftyFifty);
+            this.Controls.Add(this.lstLevel);
             this.Controls.Add(this.lblQuestion);
             this.Controls.Add(this.btnAnswerD);
             this.Controls.Add(this.btnAnswerC);
@@ -108,6 +153,7 @@
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
@@ -121,6 +167,8 @@
         private System.Windows.Forms.Button btnAnswerC;
         private System.Windows.Forms.Button btnAnswerD;
         private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.ListBox lstLevel;
+        private System.Windows.Forms.Button bntFiftyFifty;
     }
 }
 
