5dacca6cd74aa059d79bd9683c2ff706ea8b4edc
vboxuser <vboxuser@WIN10.ty>
2025-02-17 18:18:32 +0300

    12

--- WhoWantsToBeABillionaire/Form1.Designer.cs
+++ WhoWantsToBeABillionaire/Form1.Designer.cs
@@ -29,10 +29,11 @@
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.btnAnswerA = new System.Windows.Forms.Button();
+            this.btnAnswerB = new System.Windows.Forms.Button();
+            this.btnAnswerC = new System.Windows.Forms.Button();
+            this.btnAnswerD = new System.Windows.Forms.Button();
+            this.lblQuestion = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -46,51 +47,64 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // button1
+            // btnAnswerA
             // 
-            this.button1.Location = new System.Drawing.Point(98, 298);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnAnswerA.Location = new System.Drawing.Point(98, 338);
+            this.btnAnswerA.Name = "btnAnswerA";
+            this.btnAnswerA.Size = new System.Drawing.Size(75, 23);
+            this.btnAnswerA.TabIndex = 1;
+            this.btnAnswerA.Tag = "1";
+            this.btnAnswerA.Text = "button1";
+            this.btnAnswerA.UseVisualStyleBackColor = false;
             // 
-            // button2
+            // btnAnswerB
             // 
-            this.button2.Location = new System.Drawing.Point(98, 371);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnAnswerB.Location = new System.Drawing.Point(98, 386);
+            this.btnAnswerB.Name = "btnAnswerB";
+            this.btnAnswerB.Size = new System.Drawing.Size(75, 23);
+            this.btnAnswerB.TabIndex = 2;
+            this.btnAnswerB.Tag = "2";
+            this.btnAnswerB.Text = "button2";
+            this.btnAnswerB.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // btnAnswerC
             // 
-            this.button3.Location = new System.Drawing.Point(339, 298);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnAnswerC.Location = new System.Drawing.Point(341, 338);
+            this.btnAnswerC.Name = "btnAnswerC";
+            this.btnAnswerC.Size = new System.Drawing.Size(75, 23);
+            this.btnAnswerC.TabIndex = 3;
+            this.btnAnswerC.Tag = "3";
+            this.btnAnswerC.Text = "button3";
+            this.btnAnswerC.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // btnAnswerD
             // 
-            this.button4.Location = new System.Drawing.Point(339, 371);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 4;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
+            this.btnAnswerD.Location = new System.Drawing.Point(341, 386);
+            this.btnAnswerD.Name = "btnAnswerD";
+            this.btnAnswerD.Size = new System.Drawing.Size(75, 23);
+            this.btnAnswerD.TabIndex = 4;
+            this.btnAnswerD.Tag = "4";
+            this.btnAnswerD.Text = "button4";
+            this.btnAnswerD.UseVisualStyleBackColor = true;
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.Location = new System.Drawing.Point(138, 266);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(278, 41);
+            this.lblQuestion.TabIndex = 5;
+            this.lblQuestion.Text = "label1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(583, 438);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblQuestion);
+            this.Controls.Add(this.btnAnswerD);
+            this.Controls.Add(this.btnAnswerC);
+            this.Controls.Add(this.btnAnswerB);
+            this.Controls.Add(this.btnAnswerA);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -102,10 +116,11 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btnAnswerA;
+        private System.Windows.Forms.Button btnAnswerB;
+        private System.Windows.Forms.Button btnAnswerC;
+        private System.Windows.Forms.Button btnAnswerD;
+        private System.Windows.Forms.Label lblQuestion;
     }
 }
 
